cb2e2d68ab5cc02e75fc1b04b075be414e7f8ea6
strrev-dev <deepanmaurya699@gmail.com>
2025-06-19 15:45:17 +0530

    Initial commit with all Sigma 8.0 files,10 days done

---
+++ praction question/PQ3 portfolio/main.js
@@ -0,0 +1,7 @@
+lottie.loadAnimation({
+  container: document.getElementById('introAnimation'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'intro.json' // Replace with your Lottie JSON file
+});
