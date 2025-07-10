e45c929c5bd6c046b8c4f50f4436bd296c8f5e51
strrev-dev <deepanmaurya699@gmail.com>
2025-07-11 04:25:46 +0530

    Javascript classes practice and assignment day1 and 2

---
+++ JAVAscript (Js)/app.js
@@ -0,0 +1,132 @@
+// console.log(
+//     'HEllo world'
+// );
+// console.log(
+//     'deepan quantdev'
+// );
+// let a = 5;
+// let b = 4;
+// console.log('sum is : ', a + b);
+
+// // template literals:  expression used to embedded in the strings:
+
+// let orangePrice = 30;
+// let bananaPrice = 60;
+// // let fruitPrice = 'The total price is :' + (orangePrice+bananaPrice) + 'rs.';
+// let fruitPrice = `The total fruit price is : ${orangePrice + bananaPrice} rs.`;
+// console.log(fruitPrice);
+
+// //arithmetic operators
+
+// let c = 23;
+// let d = 3;
+// console.log(c + d);
+// console.log(c - d);
+// console.log(c * d);
+// console.log(c / d);
+// console.log(c % d);
+// console.log(c ** d);
+
+// //unary operators
+// console.log(c++);
+// console.log(++c);
+
+// //assignment operators
+// b = a;
+// console.log(b);
+
+// //comparison operators
+
+// let age = 28;
+// console.log(age>18);
+// console.log
+// (5==5);
+// console.log(5 !=5);
+
+// console.log('Traffic light system ');
+// let color =  'red';
+
+// if( color === 'red') {
+//     console.log('Please stop the  vehicles');
+// }
+// if( color === 'green') {
+//     console.log('you can go now');
+// }
+// if( color === 'yellow') {
+//     console.log('slow the vehicle');
+// }
+
+
+// let size = 's';
+
+// if(size === 'xl'){
+//     console.log('THe price is : 250rs.');
+// }
+// else if(size === 'l'){
+//     console.log('THe price is : 200rs.');
+// }
+// else if(size === 'm'){
+//     console.log('THe price is : 100rs.');
+// }
+// else {
+//     console.log ( 'THe price is : 50rs')
+// }
+// let name = 'anjali';
+// if ( name[0] === 'a'&& name.length > 3 ){
+//     console.log('The the strng is good string');
+// }
+// else {
+//     console.log('The string is not the good string');
+// }
+
+// switch statement 
+
+// let color = 'yellow';
+
+// switch ( color) {
+//     case 'red' : 
+//     console.log('stop');
+//     break;
+//     case 'yellow' : 
+//     console.log('drive slow');
+//     break;
+//     case 'green' : 
+//     console.log('go');
+//     break;
+//     default :
+//     console.log('light is not working');
+// }
+// let day = '4';
+
+// switch (day) {
+//     case '1':
+//         console.log('monday');
+//         break;
+
+//     case '2':
+//         console.log('tuesday');
+//         break;
+//     case '3':
+//         console.log('wednesday');
+//         break;
+//     case '4':
+//         console.log('thrusday');
+//         break;
+//     case '5':
+//         console.log('friday');
+//         break;
+//     case '6':
+//         console.log('saturday');
+//         break;
+//     case '7':
+//         console.log('sunday');
+//         break;
+// }
+
+// alert('are you sure to go further?');
+// console.log('yeah this is log');
+// console.error('this is error');
+// console.warn('this is warn line');
+
+// prompt('Enter your color name ');
+
---
+++ JAVAscript (Js)/assignment1.js
@@ -0,0 +1,61 @@
+// let num = 40;
+
+// if (num % 10 == 0) {
+//     console.log('good');
+// }
+// else {
+//     console.log('bad');
+// }
+
+// let name = prompt('Enter the name in the box');
+// let age = prompt('Enter the age');
+
+// alert(`${name} is ${age} years old`);
+// console.log(`${name} is ${age} years old`);
+
+// let quarter = 1;
+
+// switch(quarter){
+//     case 1:
+//         console.log('Months in Quarter 1 : january , february and march');
+//         break;
+//     case 2:
+//         console.log('Months in Quarter 2 : april, may and june');
+//         break;
+//     case 3:
+//         console.log('Months in Quarter 3 : july, august and september');
+//         break;
+//     case 4:
+//         console.log('Months in Quarter 4 : october, november and december');
+//         break;
+// }
+// let str = 'aanana';
+//  if((str.length > 5 )&& (str[0] == 'A' || str[0] == 'a')){
+//     console.log('this is the golden string');
+//  }
+//  else {
+//     console.log('Not a golden string');
+//  }
+// let num1 = Number(prompt('Enter the first number '));
+// let num2 = Number(prompt('Enter the second number '));
+// let num3 = Number(prompt('Enter the third number '));
+
+// if((num1>num2) && (num1>num3)){
+//     console.log(`The ${num1} is largest of three numbers`);
+// }
+// else if( (num2>num1) && num2> num3){
+//      console.log(`The ${num2} is largest of three numbers`);
+// }
+// else {
+//      console.log(`The ${num3} is largest of three numbers`);
+// }
+
+// let num1 = 32;
+// let num2 = 34352;
+
+// if( num1%10 == num2%10){
+//  console.log('The last digit of the number is :',num1%10);
+// }
+// else{
+//     console.log('The last digit are not equal');
+// }
