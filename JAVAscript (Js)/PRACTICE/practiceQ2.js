4da63c749148611d0c8e337f7406f6cfbb63cad2
strrev-dev <deepanmaurya699@gmail.com>
2025-07-12 21:40:56 +0530

    Practice 2

---
+++ JAVAscript (Js)/PRACTICE/practiceQ2.js
@@ -0,0 +1,6 @@
+let start = ['janaury', 'july','march','august'];
+console.log(start);
+start.shift();
+start.shift();
+start.unshift('july','june');
+console.log(start);
---
+++ JAVAscript (Js)/PRACTICE/practiceQs1.js
@@ -0,0 +1,10 @@
+// let msg = 'help!'
+// newMsg = msg.trim().toUpperCase();
+// console.log(newMsg);
+
+// let name = 'ApnaCollege';
+// console.log(name.slice(4,9));
+// console.log(name.indexOf('na'));
+// console.log(name.replace('Apna', 'Our'));
+// newName = name.slice(4).replace('l', 't');
+// console.log(newName);
