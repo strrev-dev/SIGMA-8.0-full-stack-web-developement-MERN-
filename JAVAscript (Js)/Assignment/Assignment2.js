a101949465870ac0f146b8ec1cf568f384aa24c8
strrev-dev <deepanmaurya699@gmail.com>
2025-07-14 01:46:46 +0530

    Assignment 2

---
+++ JAVAscript (Js)/Assignment/Assignment2.js
@@ -0,0 +1,40 @@
+// let arr  = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//  let  n = 5;
+
+//  console.log(arr.slice(0,n));
+// let arr  = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+//  let  n = 10;
+
+//  console.log(arr.slice(9,n));
+
+// let arr = [7,9,0,-2];
+// let n = 3;
+// let ans = arr.slice(arr.length-n);
+// console.log(ans);
+
+// let str = prompt("Enter a string");
+//  if (str.length == 0){
+//     console.log("String is empty");
+
+//  }
+//  else {
+//     console.log("String is not empty");
+//  }
+
+// let str = prompt("Enter a string");
+// if (str[0] == str[0].toLowerCase()) {
+//     console.log("String is in lowercase");
+// }else  {
+//     console.log("String is not in lowercase");
+// }
+    
+
+let str = '  hello world  ';
+let str1=   str.trim();
+console.log(str1); // Output: 'hello world'
+
+let students = ['Alice', 'Bob', 'Charlie', 'David'];
+
+let arr = students.includes('ob');
+console.log(arr); // Output: true
