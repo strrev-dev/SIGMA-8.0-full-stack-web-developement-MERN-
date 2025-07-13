5a02e124b81598ad4489b3c2638878aca0642444
strrev-dev <deepanmaurya699@gmail.com>
2025-07-13 05:52:14 +0530

    Practice Q 3

---
+++ JAVAscript (Js)/PRACTICE/PracticeQ3.js
@@ -0,0 +1,7 @@
+let month = ['january', 'july' ,'march', 'august'];
+console.log(month);
+console.log(month.splice(0,2, 'july', 'june'));
+
+let languages = ['c' , 'c++', 'html',  'javascript', 'python' , 'java' , 'c#' , 'sql'];
+console.log(languages);
+console.log(languages.reverse().indexOf('javascript'));
