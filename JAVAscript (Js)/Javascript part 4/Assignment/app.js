16cedda896707d93ef0dd1fce150f4ae43da9f5d
strrev-dev <deepanmaurya699@gmail.com>
2025-07-17 03:21:27 +0530

    js 4 assignment

---
+++ JAVAscript (Js)/Javascript part 4/Assignment/app.js
@@ -0,0 +1,48 @@
+// let arr = [1,2,3,4,5,6,2,3,];
+// num =2;
+// for(let i = 0;i<arr.length; i++){
+//     if(arr[i] === num){
+//         arr.splice(i,1);
+//        console.log(`Removed ${num} from the array.`);
+//        console.log(arr);
+// }
+// }
+
+// let num = 287152;
+// let count = 0;
+// copy = num;
+// while(copy>0){
+//     copy = Math.floor(copy /10);
+//     count++;
+//     console.log(count);
+// }
+
+// let num = 287152;
+// let sum = 0;
+// copy = num;
+// while(copy>0){
+//     digit = copy%10;
+//     copy = Math.floor(copy/10);
+//     sum +=digit;
+// }
+// console.log(sum);
+
+//Printing the factorial of the number.
+// let num = parseInt(prompt('Enter the number whose factorial you want to know.'));
+// let fact = 1;
+
+// for( let i=1; i<=num;i++){
+//    fact *=i;
+// }
+// console.log(`Your ${num}! factorial is ${fact}`);
+
+//find the  largest number in array with only positive integers.
+// let arr =  [2, 4,1, 5, 9, 19,12, 3, 7];
+// let largest = 0;
+//  for( let i  =0; i < arr.length; i++){
+//     if(largest< arr[i]){
+//         largest = arr[i];
+//     }
+
+//  }
+//  console.log(largest);
