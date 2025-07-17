3e8a5197a0f7a4d8ba5e4d5c8a87748f7fc368ee
strrev-dev <deepanmaurya699@gmail.com>
2025-07-17 22:51:11 +0530

    Practice and JS 5 part

---
+++ JAVAscript (Js)/JS part 5/PracticeQs.js
@@ -0,0 +1,10 @@
+//Generate the random number betweeen 1 to 100
+// Math.floor(Math.random()*100)+1;
+//Generate the random number betweeen 1 to 100
+
+// Math.floor(Math.random()*5)+1; 
+//Generate the random number betweeen 21 to 25
+// Math.floor(Math.random()*5)+21;
+
+
+
---
+++ JAVAscript (Js)/JS part 5/app.js
@@ -0,0 +1,65 @@
+// object literals
+
+// const student  = {
+// name : 'deepan',
+// age :23,
+// marks : 89.4
+// };
+
+// //Thread/twitter post storing data
+
+// let post = {
+//     username : '@deepanmaurya',
+//     feed :'This is my #first  post',
+//     likes : 150,
+//     repost:  5,
+//     Tag : ['@shubham','prabal']
+// };
+
+//get values: using obj.key or obj['key'']
+//js automatically converts the key into strings when we get the values  using object
+//Add/Update/delete values also similiar liike before we did in array .
+//obj.key = 'newvalue' ( to update the already present values)
+// obj.newkey  = 'newvalue'  ( this will add the new key with value  in it)
+// delete Object.key it will delete the present value in the object literals
+
+//nested object into objectss
+//  const classInfo = {
+//     aman : {
+//         Grade : 'A+',
+//         City : 'Delhi'
+//     },
+//     shradha : {
+//           Grade : 'A',
+//         City : 'Delhi'
+//     },
+    
+//     deepan : {
+//           Grade : 'A+',
+//         City : 'Gurgaon'
+//     },
+//     };
+//array  of objects
+
+// const classInfo = [
+// {
+//         name : 'shradha',
+//         grade : 'S+',
+//         age : 23
+//     },
+//      {
+//         name : 'shradha',
+//         grade : 'S+',
+//         age  : 23
+//     },
+//      {
+//         name : 'shradha',
+//         grade : 'S+',
+//         age  : 23
+//     },
+// ];
+//Math object
+
+
+
+ 
