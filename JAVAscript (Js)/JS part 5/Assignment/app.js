ccd9df3d27cb841b60000c091d5b9c94f03e45a2
strrev-dev <deepanmaurya699@gmail.com>
2025-07-18 00:24:10 +0530

    Assignment js part 5

---
+++ JAVAscript (Js)/JS part 5/Assignment/app.js
@@ -0,0 +1,17 @@
+//create a program to generate the  random number  representing dice roll.
+
+// const dice = Math.floor(Math.random()*6)+1;
+
+// const car = {
+//  name :'BBt',
+//  model : '1',
+//  color:  ['red','blue'  , 'black']
+// };
+
+// const Person = {
+//     name: 'deepan',
+//     age : 23,
+//     city : 'gurgaon'
+// };
+
+
